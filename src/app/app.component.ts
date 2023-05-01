78ce0ac24300d92821df626ab396b72ef811ea87
CanaanGM <reakak1@gmail.com>
2023-05-01 08:32:46 +0300

    gamer complete with reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faker} from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing_gaem';
+
+  random_sentance = faker.lorem.sentence()
+  players_text = "";
+
+  gameOver : boolean = false;
+
+
+  handleInput(userInput: any){
+    let players_input = userInput.value
+
+    this.players_text = players_input
+
+    if (this.random_sentance === this.players_text){
+      this.random_sentance = faker.lorem.sentence()
+      userInput.value = '';
+      this.players_text = '';
+    }
+  }
+
+  compare(sentance_char:string, players_char:string){
+
+    if(!players_char) return "pending";
+
+    return players_char === sentance_char
+    ? "correct"
+    : "incorrect"
+
+  }
+
 }
